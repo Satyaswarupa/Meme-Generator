12a30b515d9a1f84bb3bf257d5c92f05c34ac71f
Satyaswarupa parida <127578199+Satyaswarupa@users.noreply.github.com>
2023-10-08 01:14:20 +0530

    Update Text.jsx

--- src/components/Text.jsx
+++ src/components/Text.jsx
@@ -1,24 +1,34 @@
 import React, { useState } from "react";
 import Draggable from "react-draggable";
-import "../styles/text.scss"
+import "../styles/text.scss";
 
 const Text = () => {
   const [editMode, setEditMode] = useState(false);
-  const [value, setValue] = useState("Double Click to Edit");
+  const [value, setValue] = useState("Tap and hold to Edit"); // Update initial text
+
+  const handleLongPress = () => {
+    setEditMode(true);
+  };
 
   return (
     <Draggable>
       {editMode ? (
         <input
-          onDoubleClick={(e) => setEditMode(false)}
+          onBlur={(e) => setEditMode(false)} // Use onBlur to exit edit mode on mobile
           value={value}
           onChange={(e) => setValue(e.target.value)}
         />
       ) : (
-        <h1 className="text" onDoubleClick={(e) => setEditMode(true)}>{value}</h1>
+        <h1
+          className="text"
+          onTouchStart={handleLongPress} // Use onTouchStart for tap-and-hold gesture
+        >
+          {value}
+        </h1>
       )}
     </Draggable>
   );
 };
 
 export default Text;
+
