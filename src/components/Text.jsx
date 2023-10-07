b9bcd9003992d664fbd28c1803605c5921100fc9
Satyaswarupa parida <127578199+Satyaswarupa@users.noreply.github.com>
2023-10-08 01:38:43 +0530

    Update Text.jsx

--- src/components/Text.jsx
+++ src/components/Text.jsx
@@ -1,38 +1,21 @@
 import React, { useState } from "react";
 import Draggable from "react-draggable";
-import "../styles/text.scss";
+import "../styles/text.scss"
 
 const Text = () => {
   const [editMode, setEditMode] = useState(false);
-  const [value, setValue] = useState("Tap and hold to Edit");
-
-  const handleLongPress = () => {
-    setEditMode(true);
-  };
-
-  const handleSingleTouch = () => {
-    if (!editMode) {
-      // Only toggle edit mode if not already in edit mode
-      setEditMode(!editMode);
-    }
-  };
+  const [value, setValue] = useState("Double Click to Edit");
 
   return (
     <Draggable>
       {editMode ? (
         <input
-          onBlur={(e) => setEditMode(false)}
+          onDoubleClick={(e) => setEditMode(false)}
           value={value}
           onChange={(e) => setValue(e.target.value)}
         />
       ) : (
-        <h1
-          className="text"
-          onTouchStart={handleLongPress}
-          onTouchEnd={handleSingleTouch} // Use onTouchEnd for single touch
-        >
-          {value}
-        </h1>
+        <h1 className="text" onDoubleClick={(e) => setEditMode(true)}>{value}</h1>
       )}
     </Draggable>
   );
